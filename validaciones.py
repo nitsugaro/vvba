dd4782874f9ac94f91c887b97384938b782aae26
nitsugaro <agusromero0815@gmail.com>
2025-09-10 21:00:45 -0300

    updated

--- validaciones.py
+++ validaciones.py
@@ -1,36 +1,24 @@
+import db
+import db.usuarios
 
-def validarOpcionMenuInicio(intNum):
-    while intNum != 1 and intNum != 2:
-        print("Error. Ingrese un 1 o un 2.")
-        intNum = int(input("Ingrese 1 para iniciar sesion o 2 para crear un usuario: "))
-    return intNum
-
-def validarOpcionMenuPrincipal(intNum, lstOpciones):
-    while 1 > intNum or intNum > len(lstOpciones):
-        print("Error. Ingrese un numero correspondiente con las opciones.\n" \
-        "1. Consultar saldo \n2. Depositar dinero \n3. Movimientos \n4. Creditos \n5. Plazo fijo \n6. Compra/Venta Dolar \n7. Gastos por clasificacion \n8. Salir")
-        
-        intNum = int(input("Elija una opción: "))
-    return intNum
-
-def validarNombreUsuario(strUsuario, matrizUsuario):
+def validarNombreUsuario(strUsuario):
     while True: 
-        if strUsuario in matrizUsuario[1]:
+        if db.usuarios.obtenerIndexUsuario(strUsuario) != -1:
             strUsuario = input(f"El nombre de usuario {strUsuario} ya existe. Cree otro nombre de usuario: ")
-        else:
-            matrizUsuario[1].append(strUsuario)
+        else: 
             break
 
-def validarContraseña(strContraseña, matrizUsuario):
+    return strUsuario
+
+def validarContraseña(strContraseña):
     while True:
-        if strContraseña in matrizUsuario[2]:
-            strContraseña = input("Cree otra contraseña: ")
-        elif len(strContraseña) < 5 or not any(i.isdigit() for i in strContraseña):
+        if len(strContraseña) < 5 or not any(i.isdigit() for i in strContraseña):
             print("La contraseña tiene que estar formada por almenos 5 caracteres y tener un numero.")
             strContraseña = input("Cree otra contraseña: ")
-        else: 
-            matrizUsuario[2].append(strContraseña)
+        else:
             break
+
+    return strContraseña
 
 def validarNumeroPositivo(intNum):
     while intNum <= 0:
